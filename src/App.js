7b53eb41d458606925d92b4f65c87a6e04978579
Kalvin <kalvinhart@hotmail.co.uk>
2022-01-11 13:08:10 +0000

    Added Router with first route.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import GlobalStyles from "./GlobalStyles";
+import Routes from "./Routes";
 import Table from "./components/Table/Table";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <GlobalStyles />
-      <Table />
-    </>
+      <Routes />
+    </BrowserRouter>
   );
 }
 
