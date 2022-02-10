c6f5cca667d923ed5d59837030214f7c4e840e4f
Kalvin <kalvinhart@hotmail.co.uk>
2022-02-10 14:17:08 +0000

    Moved sign out UI to Header component.

--- src/components/jobs/JobsPage.js
+++ src/components/jobs/JobsPage.js
@@ -6,16 +6,14 @@ import { useNavigate } from "react-router-dom";
 import Table from "./Table/Table";
 import NoData from "./Table/NoData";
 import Spinner from "../shared/Spinner/Spinner";
-import UserInfo from "../shared/UserInfo/UserInfo";
 
 import { columnData } from "../../tableConfig";
 
 const JobsPage = () => {
   const navigate = useNavigate();
   const [loading, setLoading] = useState(true);
-  const { clearAllJobStates, jobs, getJobs, jobsForTable, setSelectedJob } =
-    useContext(JobContext);
-  const { userID, signOut } = useContext(AuthContext);
+  const { jobs, getJobs, jobsForTable, setSelectedJob } = useContext(JobContext);
+  const { userID } = useContext(AuthContext);
   const columns = useMemo(() => columnData, []);
   const data = useMemo(() => jobsForTable, [jobsForTable]);
 
@@ -38,17 +36,10 @@ const JobsPage = () => {
     navigate(`/job/${id}`);
   };
 
-  const signUserOut = () => {
-    signOut();
-    clearAllJobStates();
-    navigate("/signin");
-  };
-
   if (loading) return <Spinner />;
 
   return (
     <>
-      <UserInfo signOut={signUserOut} />
       {!loading && jobs.length === 0 ? (
         <NoData />
       ) : (
--- src/components/shared/Header/Header.js
+++ src/components/shared/Header/Header.js
@@ -1,11 +1,29 @@
-import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { JobContext } from "../../../context/jobContext";
+import { AuthContext } from "../../../context/authContext";
+
+import { Link, useNavigate } from "react-router-dom";
+
+import UserInfo from "../UserInfo/UserInfo";
 
 import { StyledHeader } from "./Header.styled";
 import { H1 } from "../../../styles/fontStyles";
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const { clearAllJobStates } = useContext(JobContext);
+  const { signOut } = useContext(AuthContext);
+
+  const signUserOut = () => {
+    signOut();
+    clearAllJobStates();
+    navigate("/signin");
+  };
+
   return (
     <StyledHeader>
+      <UserInfo signOut={signUserOut} />
       <Link to="/">
         <H1>Job Application Tracker</H1>
       </Link>
