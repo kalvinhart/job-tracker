ae973a05404b567c14d48e6a0ef93f603dbe4ba6
Kalvin <kalvinhart@hotmail.co.uk>
2022-02-15 14:06:44 +0000

    Ameded styling for tags.

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -11,7 +11,7 @@ const GlobalStyle = createGlobalStyle`
 
     :root {
         --clr-primary: #f25b86;
-        --clr-primary-rgba: rgba(242, 91, 134, 0.5);
+        --clr-primary-rgba: rgba(242, 91, 134, 0.2);
         --clr-secondary: #2a2a2a;
         --clr-tertiary: #636e72;
         --clr-tertiary-text: #00a8ff;
