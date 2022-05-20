06ac9af789f64f288baa65125c077a7afe664b33
Kalvin <kalvinhart@hotmail.co.uk>
2022-05-20 22:48:19 +0100

    Refactored to utilise custom hook.

--- src/hooks/shared/useAuthActions/useAuthActions.js
+++ src/hooks/shared/useAuthActions/useAuthActions.js
@@ -1,19 +1,18 @@
 import { useContext } from "react";
-import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router";
 import { AuthContext } from "../../../context/authContext";
 
-import { clearJobState } from "../../../slices/jobSlice";
+import { useJobSlice } from "../useJobSlice/useJobSlice";
 
 export const useAuthActions = () => {
-  const dispatch = useDispatch();
   const { signUp, signIn, signOut, resetPassword } = useContext(AuthContext);
+  const { clearJobState } = useJobSlice();
 
   const navigate = useNavigate();
 
   const signUserOut = () => {
     signOut();
-    dispatch(clearJobState());
+    clearJobState();
     navigate("/signin");
   };
 
