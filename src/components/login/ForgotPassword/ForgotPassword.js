b2f910eb29cbb17b863d08ee490b4bceb10c370c
Kalvin <kalvinhart@hotmail.co.uk>
2022-05-20 22:49:28 +0100

    Added tests.

--- src/components/login/ForgotPassword/ForgotPassword.js
+++ src/components/login/ForgotPassword/ForgotPassword.js
@@ -1,15 +1,15 @@
 import { StyledInput, StyledInputGroup, StyledLabel } from "../../../styles/formStyles";
 import { StyledBackgroundDiv, StyledForm } from "../../shared/AuthForm/AuthForm.styled";
-import { H2 } from "../../../styles/fontStyles";
+import { ErrorSpan, H2 } from "../../../styles/fontStyles";
 import { Button } from "../../../styles/buttonStyles";
 import { useForgotPassword } from "../../../hooks/useForgotPassword/useForgotPassword";
 
 const ForgotPassword = () => {
-  const { email, handleChange, handleSubmit } = useForgotPassword();
+  const { email, error, handleChange, handleSubmit } = useForgotPassword();
 
   return (
     <StyledBackgroundDiv>
-      <StyledForm onSubmit={handleSubmit}>
+      <StyledForm onSubmit={handleSubmit} aria-label="Reset your Password">
         <H2>Reset your Password</H2>
         <StyledInputGroup>
           <StyledLabel htmlFor="email">Email Address:</StyledLabel>
@@ -21,6 +21,7 @@ const ForgotPassword = () => {
             onChange={handleChange}
           />
         </StyledInputGroup>
+        {error && <ErrorSpan inline>{error}</ErrorSpan>}
         <Button primary type="submit">
           Reset
         </Button>
