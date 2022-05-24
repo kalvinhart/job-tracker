971129c5f2252316a657e69c3372aa57bd4e3e38
Kalvin <kalvinhart@hotmail.co.uk>
2022-05-24 17:52:21 +0100

    Added tests.

--- src/hooks/useForgotPassword/useForgotPassword.js
+++ src/hooks/useForgotPassword/useForgotPassword.js
@@ -3,22 +3,30 @@ import { useAuthActions } from "../shared/useAuthActions/useAuthActions";
 
 export const useForgotPassword = () => {
   const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
   const { resetPassword } = useAuthActions();
 
   const handleChange = (e) => {
+    if (error) {
+      setError("");
+    }
     setEmail(e.target.value);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (!email) return;
+    if (!email) {
+      setError("You must enter an email address.");
+      return;
+    }
 
     resetPassword(email);
   };
 
   return {
     email,
+    error,
     handleChange: (e) => handleChange(e),
     handleSubmit: (e) => handleSubmit(e),
   };
