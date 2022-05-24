335f95e5f4dfc33ec2d5495c86d8791d8d9c436f
Kalvin <kalvinhart@hotmail.co.uk>
2022-05-24 18:06:32 +0100

    Added lazy loading for all routes.

--- src/Routes.js
+++ src/Routes.js
@@ -1,25 +1,72 @@
+import { lazy, Suspense } from "react";
 import { Routes as RoutesList, Route } from "react-router-dom";
 
+import Spinner from "./components/shared/Spinner/Spinner";
+
 import PrivateRoute from "./components/shared/PrivateRoute/PrivateRoute";
-import JobPage from "./components/job/JobPage";
-import JobsPage from "./components/jobs/JobsPage";
-import LoginPage from "./components/login/LoginPage";
-import RegisterPage from "./components/register/RegisterPage";
-import ForgotPassword from "./components/login/ForgotPassword/ForgotPassword";
+const JobPage = lazy(() => import("./components/job/JobPage"));
+const JobsPage = lazy(() => import("./components/jobs/JobsPage"));
+const LoginPage = lazy(() => import("./components/login/LoginPage"));
+const RegisterPage = lazy(() => import("./components/register/RegisterPage"));
+const ForgotPassword = lazy(() =>
+  import("./components/login/ForgotPassword/ForgotPassword")
+);
 
 const Routes = () => {
   return (
     <RoutesList>
-      <Route path="/signin" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route
+        path="/signin"
+        element={
+          <Suspense fallback={Spinner}>
+            <LoginPage />
+          </Suspense>
+        }
+      />
+      <Route
+        path="/register"
+        element={
+          <Suspense fallback={Spinner}>
+            <RegisterPage />
+          </Suspense>
+        }
+      />
+      <Route
+        path="/forgot-password"
+        element={
+          <Suspense fallback={Spinner}>
+            <ForgotPassword />
+          </Suspense>
+        }
+      />
 
       <Route path="/" element={<PrivateRoute />}>
-        <Route path="job-list" element={<JobsPage />} />
+        <Route
+          path="job-list"
+          element={
+            <Suspense fallback={Spinner}>
+              <JobsPage />
+            </Suspense>
+          }
+        />
 
-        <Route path="/" element={<JobsPage />} />
+        <Route
+          path="/"
+          element={
+            <Suspense fallback={Spinner}>
+              <JobsPage />
+            </Suspense>
+          }
+        />
 
-        <Route path="job/:id" element={<JobPage />} />
+        <Route
+          path="job/:id"
+          element={
+            <Suspense fallback={Spinner}>
+              <JobPage />
+            </Suspense>
+          }
+        />
       </Route>
     </RoutesList>
   );
