65b99afd1b0e382e1bcc127eef607860df761884
Kalvin <kalvinhart@hotmail.co.uk>
2022-05-24 17:57:50 +0100

    Refactored layout of components.

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,6 @@
 import { Routes as RoutesList, Route } from "react-router-dom";
+
 import PrivateRoute from "./components/shared/PrivateRoute/PrivateRoute";
-import Layout from "./components/shared/Layout/Layout";
 import JobPage from "./components/job/JobPage";
 import JobsPage from "./components/jobs/JobsPage";
 import LoginPage from "./components/login/LoginPage";
@@ -15,32 +15,11 @@ const Routes = () => {
       <Route path="/forgot-password" element={<ForgotPassword />} />
 
       <Route path="/" element={<PrivateRoute />}>
-        <Route
-          path="job-list"
-          element={
-            <Layout>
-              <JobsPage />
-            </Layout>
-          }
-        />
+        <Route path="job-list" element={<JobsPage />} />
 
-        <Route
-          path="/"
-          element={
-            <Layout>
-              <JobsPage />
-            </Layout>
-          }
-        />
+        <Route path="/" element={<JobsPage />} />
 
-        <Route
-          path="job/:id"
-          element={
-            <Layout>
-              <JobPage />
-            </Layout>
-          }
-        />
+        <Route path="job/:id" element={<JobPage />} />
       </Route>
     </RoutesList>
   );
