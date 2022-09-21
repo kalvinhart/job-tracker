ff88e2a3527e663d1f912670efca80e790e8ca83
Kalvin <kalvinhart@hotmail.co.uk>
2022-09-21 19:19:13 +0100

    refactor: sorting logic now happens in reducer

--- src/features/list-jobs/hooks/useJobList.ts
+++ src/features/list-jobs/hooks/useJobList.ts
@@ -1,4 +1,4 @@
-import { SyntheticEvent, useCallback, useMemo, useState } from "react";
+import { SyntheticEvent, useState } from "react";
 
 import { useJobSlice } from "../../../common/hooks/useJobSlice/useJobSlice";
 import { useJobListContext } from "./useJobListContext";
@@ -21,16 +21,8 @@ export const useJobList = () => {
     await deleteManyJobs(selectedJobs);
   };
 
-  const renderJobs = useCallback(() => {
-    if (selectedTab === "All") return jobs;
-
-    return jobs!
-      .filter((job) => job.status === selectedTab)
-      .sort((a, b) => +a.date - +b.date);
-  }, [jobs, selectedTab]);
-
-  let jobsToDisplay: Job[] = useMemo(() => renderJobs(), [renderJobs]);
-  console.log(jobsToDisplay);
+  const jobsToDisplay: Job[] =
+    selectedTab === "All" ? jobs : jobs.filter((job) => job.status === selectedTab);
 
   return {
     jobsToDisplay,
