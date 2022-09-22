8aa842def26cd52dc21d79a26df10abaae3ff198
Kalvin <kalvinhart@hotmail.co.uk>
2022-09-22 13:55:19 +0100

    refactor: changed date format

--- src/common/utilities/formatDate.ts
+++ src/common/utilities/formatDate.ts
@@ -1,5 +1,39 @@
-export const toDateString = (date: number) => {
-  return new Date(date).toDateString();
+type Key = {
+  [key: string]: string;
+};
+
+const DATE_SUFFIX: Key = {
+  1: "st",
+  2: "nd",
+  3: "rd",
+  other: "th",
+};
+
+const MONTHS: Key = {
+  0: "January",
+  1: "February",
+  2: "March",
+  3: "April",
+  4: "May",
+  5: "June",
+  6: "July",
+  7: "August",
+  8: "September",
+  9: "October",
+  10: "November",
+  11: "December",
+};
+
+export const toDateString = (time: number) => {
+  const date = new Date(time);
+  const dateNum = date.getDate();
+  const month = date.getMonth();
+
+  const dateSuffix = dateNum <= 3 ? DATE_SUFFIX[dateNum] : DATE_SUFFIX["other"];
+
+  const dateString = `${dateNum}${dateSuffix} ${MONTHS[month]}`;
+
+  return dateString;
 };
 
 export const formatTime = (time: number) => {
