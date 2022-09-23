730ed876a77fde611aee94b6d574ab3da3b75244
Kalvin <kalvinhart@hotmail.co.uk>
2022-09-23 12:25:07 +0100

    created hook for handling media query logic

---
+++ src/common/hooks/useMediaQuery/useMediaQuery.ts
@@ -0,0 +1,40 @@
+import { useCallback, useEffect, useState } from "react";
+
+export const useMediaQuery = (query: string): boolean => {
+  const getMatches = (query: string): boolean => {
+    if (typeof window !== "undefined") {
+      return window.matchMedia(query).matches;
+    }
+    return false;
+  };
+
+  const [matches, setMatches] = useState<boolean>(getMatches(query));
+
+  console.log(query, matches);
+
+  const handleChange = useCallback(() => {
+    setMatches(getMatches(query));
+  }, [query]);
+
+  useEffect(() => {
+    const matchTest = window.matchMedia(query);
+
+    handleChange();
+
+    if (matchTest.addListener) {
+      matchTest.addListener(handleChange);
+    } else {
+      matchTest.addEventListener("change", handleChange);
+    }
+
+    return () => {
+      if (matchTest.removeListener) {
+        matchTest.removeListener(handleChange);
+      } else {
+        matchTest.removeEventListener("change", handleChange);
+      }
+    };
+  }, [query, handleChange]);
+
+  return matches;
+};
