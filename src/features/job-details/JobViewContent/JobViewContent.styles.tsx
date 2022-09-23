71d70bf76d0760a9e743d863f2f0ffbc077d3ae3
Kalvin <kalvinhart@hotmail.co.uk>
2022-09-23 13:10:28 +0100

    style: added mobile styles

--- src/features/job-details/JobViewContent/JobViewContent.styles.tsx
+++ src/features/job-details/JobViewContent/JobViewContent.styles.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { Background } from "../../../common/styles/bgStyles";
 import { SpanBoldLarge } from "../../../common/styles/fontStyles";
+import { mediaSizes } from "../../../common/styles/media";
 
 export const JobViewContentWrapper = styled.div`
   width: 100%;
@@ -33,8 +34,13 @@ export const JobSpecificsWrapper = styled.div`
   padding: 20px;
   margin-bottom: 20px;
   display: flex;
+  flex-direction: column;
   background-color: var(--clr-bg);
   border-radius: var(--border-radius);
+
+  @media screen and (min-width: ${mediaSizes.large}) {
+    flex-direction: row;
+  }
 `;
 
 export const JobSpecificsGroup = styled.div`
@@ -42,7 +48,14 @@ export const JobSpecificsGroup = styled.div`
   flex-direction: column;
 
   &:not(:last-child) {
-    margin-right: 100px;
+    margin-bottom: 10px;
+  }
+
+  @media screen and (min-width: ${mediaSizes.large}) {
+    &:not(:last-child) {
+      margin-right: 100px;
+      margin-bottom: 0;
+    }
   }
 `;
 
--- src/features/job-details/JobViewHeader/JobViewHeader.styles.tsx
+++ src/features/job-details/JobViewHeader/JobViewHeader.styles.tsx
@@ -1,9 +1,14 @@
 import styled from "styled-components";
+import { mediaSizes } from "../../../common/styles/media";
 
 export const JobViewHeaderWrapper = styled.div`
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   margin-bottom: 20px;
+
+  @media screen and (min-width: ${mediaSizes.large}) {
+    justify-content: flex-start;
+  }
 `;
 
 export const JobViewHeaderGroup = styled.div`
