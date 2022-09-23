e7f65166c63874f882a997b1933c93aed0860f17
Kalvin <kalvinhart@hotmail.co.uk>
2022-09-23 17:05:55 +0100

    chore: removed redundant button

--- src/features/list-jobs/components/NoData/NoData.tsx
+++ src/features/list-jobs/components/NoData/NoData.tsx
@@ -1,21 +1,13 @@
-import { useUiSlice } from "../../../../common/hooks/useUiSlice/useUiSlice";
-
-import { Background } from "../../../../common/styles/bgStyles";
-import { Button } from "../../../../common/styles/buttonStyles";
-import { H2, Paragraph } from "../../../../common/styles/fontStyles";
 import { NoDataWrapper } from "./NoData.styles";
+import { Background } from "../../../../common/styles/bgStyles";
+import { H2, Paragraph } from "../../../../common/styles/fontStyles";
 
 const NoData = () => {
-  const { openSidePanel } = useUiSlice();
-
   return (
     <Background>
       <NoDataWrapper>
         <H2>No data found</H2>
         <Paragraph>You currently have no saved jobs.</Paragraph>
-        <Button variant="primary" onClick={openSidePanel}>
-          Add a job
-        </Button>
       </NoDataWrapper>
     </Background>
   );
